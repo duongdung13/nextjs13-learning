f7662007ab7a8370c7ee22ec7171092308e14685
Duong Dung <dungdcth@gmail.com>
2025-05-02 22:41:24 +0700

    Create TempBranch.tsx
    Create new file temp

---
+++ src/TempBranch.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function TempBranch() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Hello world with Nextjs 13 & Hỏi Dân IT
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={200}
+          height={50}
+          priority
+        />
+      </div>
+    </main>
+  )
+}
